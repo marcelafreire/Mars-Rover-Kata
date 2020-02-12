c4cb81e97ef9affc2e19b198896e0d5be83ec071
“Marcela <marcelavieirafreire@gmail.com>
2020-02-12 12:03:21 -0300

    Exercicio mars rover atualizado

--- mars-rover.js
+++ mars-rover.js
@@ -2,11 +2,28 @@ const rover = {
   direction : "N",
   x: 0,
   y: 0,
-  travelLog: []
+  travelLog: [{y:0, x:0}]
 }
 
-function turnLeft() {
-  	    switch(rover.direction){
+let board = [
+  [0, "*", 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+  [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+]
+
+function turnLeft(rover) {
+  
+     switch(rover.direction){
       case "N": 
         rover.direction = "W";
         break;
@@ -19,83 +36,120 @@ function turnLeft() {
       case "E": 
         rover.direction = "N";
         break;  
-}
- console.log(`${rover.direction}`);
-}
+} 
+  return `${rover.direction}`
+} 
 
 
-function turnRight(){
-      	switch(rover.direction){
+
+function turnRight(rover){
+        switch(rover.direction){
       case "W": 
         rover.direction = "N";
         break;
-      case "S": // SOUTH
+      case "S": 
         rover.direction = "W";
         break;
-      case "E": // EAST
+      case "E":
         rover.direction = "S";
         break;
-      case "N": // NORTH
+      case "N": 
         rover.direction = "E";
         break;
-        }
-  console.log(`Direction : ${rover.direction}`);
+        }  
+   return `${rover.direction}`
+
 }
 
-function moveForward(rover){
+
+
+function moveForward(rover, board){
   switch(rover.direction){
       case "W": 
+      if (board[rover.y][rover.x - 1] === "*") {
+        console.log(`you shall not pass`);
+        } else {
         rover.x--
+        }
         break;
-      case "N": 
+    case "N": 
+    if (board[rover.y - 1][rover.x] === "*") {
+      console.log(`you shall not pass`);
+      } else {
         rover.y--
-      case "S": 
+      }
+        break;
+    case "S": 
+    if (board[rover.y + 1][rover.x] === "*") {
+      console.log(`you shall not pass`);
+      } else {
         rover.y++
+      }
         break;
+    case "E":
+      if (board[rover.y][rover.x + 1] === "*") {
+        console.log(`you shall not pass`);
+        } else {
+        rover.x++
+        }
+        break;
+      }
 
-}
-  console.log(rover.direction);
-}
+  return `${rover.direction}`
+}     
 
-function moveBackward(){
+
+
+function moveBackward(rover){
   switch(rover.direction){
-  case "N" :
-  rover.y ++
-  break;
-  case "S" :
-  rover.y --
-  break;
-  case "W" : 
-  rover.x ++
-  break;
-  case "E" :
-  rover.x --
-  break;}
+    case "N" :
+      rover.y++
+      break;
+    case "S" :
+      rover.y--
+      break;
+    case "W" : 
+      rover.x++
+      break;
+    case "E" :
+      rover.x--
+      break;
+  }
+  return `${rover.direction}`
 }
 
-  
-function commands(list) {
-  for(let i = 0; i < list.length; i++) 
-    switch(order[i]) {
-      case "l": 
-      turnLeft();
-        rover.travelLog.push(rover);
-        break;
-      case "r":
-        turnRigth();
-        rover.travelLog.push(rover);
-        break;
-      case "f"
-        moveFoward();
-         rover.travelLog.push(rover);
-        break;
-      case "b"
-        moveBackward();
-        rover.travelLog.push(rover);
-        break;
-        
-    }
-};
 
-commands("rffrfflfrff");
+
+function commands(list) {
+
+  for(let i = 0; i < list.length; i++) { 
+       switch(list[i]) {
+         case "l": 
+           turnLeft(rover);
+           break;
+         case "r":
+           turnRight(rover);
+           break;
+         case "f":
+          moveForward(rover, board);
+          break;
+         case "b":
+          moveBackward(rover);
+          break;
+          default:
+            console.log(`insira um comando correto`)
+            break;
+          }
+}
+let newPosition = {y: rover.y, x: rover.x};
+    rover.travelLog.push(newPosition);
+if (newPosition.y > 9 || newPosition.y < 0 || newPosition.x < 0 || newPosition.x > 9) {
+  console.log(`você está fora do quadro`);
+} else {
+return rover.travelLog
+}
+}
+
+console.log(commands("rf"));
+
 
